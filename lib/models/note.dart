0ac11ece625f817b073c45649ee9e652da0e1750
nguyenquan010320 <116538457+nguyenquan010320@users.noreply.github.com>
2026-04-02 20:10:58 +0700

    Clean up comments in note.dart
    Removed comments for clarity in the Note class.

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -6,21 +6,17 @@ class Note {
 
   Note({required this.title, required this.content});
 
-  // convert object → map
   Map<String, dynamic> toMap() {
     return {'title': title, 'content': content};
   }
 
-  // map → object
   factory Note.fromMap(Map<String, dynamic> map) {
     return Note(title: map['title'], content: map['content']);
   }
 
-  // list → json string
   static String encode(List<Note> notes) =>
       jsonEncode(notes.map((n) => n.toMap()).toList());
 
-  // json string → list
   static List<Note> decode(String notes) => (jsonDecode(notes) as List<dynamic>)
       .map((item) => Note.fromMap(item))
       .toList();
